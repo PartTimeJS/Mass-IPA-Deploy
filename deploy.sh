7c32e5fb57488ce7f291fe535ac8ca2c05d9dab4
PartTimeJS <r.g.levasseur@gmail.com>
2019-10-06 18:13:58 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,21 @@
 #!/usr/local/bin/bash
+
+echo "[deploy.sh] Unpacking ipa"
+unzip ./resigned.ipa
+
 mapfile -t devices < <( idevice_id -l )
 
 for i in "${devices[@]}"
 do
-echo "updating ${i}"
-ipa-deploy resigned.ipa -i ${i}
+
+  echo "[deploy.sh] Removing UI Tests from ${i}"
+
+  ios-deploy --id ${i} --uninstall_only --bundle_id com.apple.test.RealDeviceMap-UIControlUITests-Runner
+
+  echo "[deploy.sh] Deploying Updated App to${i}"
+
+  ios-deploy -b ./Payload/*.app -i ${i}
+
 done
+
+rm -rf ./Payload
